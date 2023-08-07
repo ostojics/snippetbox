c5aaa412ee638300dd2ef5380c27ce90fcfeda6a
ostojics <ostojic.dev@gmail.com>
2023-08-07 14:53:08 +0200

    add db queries

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,28 +1,56 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql" // New import
+	"github.com/ostojics/snippetbox/internal/models"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 func main() {
 	port := flag.String("port", ":8000", "HTTP network port")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	server := &http.Server{
@@ -32,7 +60,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s...", *port)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 
 	errorLog.Fatal(err)
 }
